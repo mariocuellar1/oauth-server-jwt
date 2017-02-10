4167591d071045f402092bdb5dafd250ad0ae433
Mario Cuellar <mariocuellar1@gmail.com>
2017-02-10 14:33:41 +0100

    Se elimina wro

--- src/main/java/io/mcore/ride/sharing/oauth/AuthserverApplication.java
+++ src/main/java/io/mcore/ride/sharing/oauth/AuthserverApplication.java
@@ -6,15 +6,13 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.ManagementServerProperties;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -33,12 +31,9 @@ import org.springframework.security.oauth2.provider.ClientRegistrationException;
 import org.springframework.security.oauth2.provider.client.BaseClientDetails;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import io.mcore.ride.sharing.oauth.model.AppClient;
@@ -46,21 +41,18 @@ import io.mcore.ride.sharing.oauth.model.AppUser;
 import io.mcore.ride.sharing.oauth.repository.AppClientsRepository;
 import io.mcore.ride.sharing.oauth.repository.AppUsersRepository;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
-@Controller
-@SessionAttributes("authorizationRequest")
+@SpringBootApplication
+@RestController
+@EnableResourceServer
 public class AuthserverApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthserverApplication.class, args);
 	}
-
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-		registry.addViewController("/login").setViewName("login");
-		registry.addViewController("/oauth/confirm_access").setViewName("authorize");
+	
+	@RequestMapping("/user")
+	public Principal user(Principal user) {
+		return user;
 	}
 
 	@Configuration
@@ -69,13 +61,6 @@ public class AuthserverApplication extends WebMvcConfigurerAdapter {
 
 		@Autowired
 		AppUserDetailsService userDetailsService;
-
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.formLogin().loginPage("/login").permitAll().
-			and().authorizeRequests().anyRequest().authenticated();
-
-		}
 
 		@Override
 		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
